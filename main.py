ceaadbc880af8c5ca1184cb48cfdd28f426d886c
Yashaswini Shivathaya <83407675+y-ashaswini@users.noreply.github.com>
2022-10-02 02:02:36 +0530

    remove allow all methods

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["*"],
+#     allow_methods=["*"],
     allow_headers=["*"],
 )
 
